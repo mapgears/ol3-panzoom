96813430e2007f4d2dcc1ba7fa2f17b3f8e2ed98
Alexandre Dubé <adube@mapgears.com>
2015-10-28 09:29:41 -0400

    Add example with panzoombar on the right

---
+++ examples/panzoombar-right.js
@@ -0,0 +1,35 @@
+var minZoom = 1;
+var maxZoom = 19;
+
+/**
+ * In OpenLayers 2, the OpenLayers.ImgPath was used to define a directory where
+ * native controls fetched their images. The same idea is borrowed here in
+ * order to reuse the same images.
+ */
+var panZoom = new ol.control.PanZoom({
+  className: 'olControlPanZoomBar',
+  imgPath: './resources/ol2img',
+  minZoom: minZoom,
+  maxZoom: maxZoom,
+  slider: true
+});
+
+var map = new ol.Map({
+  controls: ol.control.defaults({
+    zoom: false
+  }).extend([
+    panZoom
+  ]),
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  target: 'map',
+  view: new ol.View({
+    center:[-7910321, 6179398],
+    minZoom: minZoom,
+    maxZoom: maxZoom,
+    zoom: 12
+  })
+});
